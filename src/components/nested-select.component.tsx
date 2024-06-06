3f6b81cdf4eeedca79d902a8e3885b88d373141c
Leticia de la Osa <deletidev@gmail.com>
2024-06-06 17:37:12 +0200

    displays the list only if isOpen is true

--- src/components/nested-select.component.tsx
+++ src/components/nested-select.component.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { TreeOption } from "../app";
+import React from 'react';
+import { TreeOption } from '../app';
 import {
   A11yNestedListOption,
   useA11yNestedSelect,
-  mapNestedSelectOptionsToFlatOptions,
-} from "../a11y";
-import { NestedOptions } from "./nested-options.component";
+  mapNestedSelectOptionsToFlatOptions
+} from '../a11y';
+import { NestedOptions } from './nested-options.component';
 
 interface Props {
   options: TreeOption[];
@@ -29,10 +29,10 @@ export const findPath = (
       currentId = undefined;
     }
   }
-  return path.join(" > ");
+  return path.join(' > ');
 };
 
-export const NestedSelect: React.FC<Props> = (props) => {
+export const NestedSelect: React.FC<Props> = props => {
   const {
     optionListRef,
     buttonRef,
@@ -41,8 +41,8 @@ export const NestedSelect: React.FC<Props> = (props) => {
     setIsOpen,
     onFocusOption,
     selectedOption,
-    setSelectedOption,
-  } = useA11yNestedSelect(props.options, (option) => option.id);
+    setSelectedOption
+  } = useA11yNestedSelect(props.options, option => option.id);
 
   const selectedPath = findPath(selectedOption?.id, options);
 
@@ -61,38 +61,39 @@ export const NestedSelect: React.FC<Props> = (props) => {
         ref={buttonRef}
         onClick={() => setIsOpen(!isOpen)}
       >
-        {selectedOption ? selectedPath : "Select an option"}
-        <ul
-          id="listbox1"
-          role="tree"
-          aria-labelledby="combo1-label"
-          tabIndex={-1}
-          style={{ display: isOpen ? "block" : "none" }}
-          ref={optionListRef}
-        >
-          {options.map((option) => {
-            return option.children ? (
-              <NestedOptions
-                handleSelectOption={setSelectedOption}
-                option={option}
-                selectedOption={selectedOption}
-                onFocusOption={onFocusOption}
-                key={option.id}
-              ></NestedOptions>
-            ) : (
-              <li
-                key={option.id}
-                role="treeitem"
-                tabIndex={option.tabIndex}
-                aria-selected={selectedOption?.label === option.label}
-                onClick={() => setSelectedOption(option.id)}
-                ref={onFocusOption(option)}
-              >
-                {option.label}
-              </li>
-            );
-          })}
-        </ul>
+        {selectedOption ? selectedPath : 'Select an option'}
+        {isOpen && (
+          <ul
+            id="listbox1"
+            role="tree"
+            aria-labelledby="combo1-label"
+            tabIndex={-1}
+            ref={optionListRef}
+          >
+            {options.map(option => {
+              return option.children ? (
+                <NestedOptions
+                  handleSelectOption={setSelectedOption}
+                  option={option}
+                  selectedOption={selectedOption}
+                  onFocusOption={onFocusOption}
+                  key={option.id}
+                ></NestedOptions>
+              ) : (
+                <li
+                  key={option.id}
+                  role="treeitem"
+                  tabIndex={option.tabIndex}
+                  aria-selected={selectedOption?.label === option.label}
+                  onClick={() => setSelectedOption(option.id)}
+                  ref={onFocusOption(option)}
+                >
+                  {option.label}
+                </li>
+              );
+            })}
+          </ul>
+        )}
       </button>
     </div>
   );
--- src/components/select.component.tsx
+++ src/components/select.component.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useA11ySelect } from "../a11y";
+import React from 'react';
+import { useA11ySelect } from '../a11y';
 
 interface Option {
   id: string;
@@ -8,7 +8,7 @@ interface Option {
 interface Props {
   options: string[];
 }
-export const Select: React.FC<Props> = (props) => {
+export const Select: React.FC<Props> = props => {
   const {
     optionListRef,
     buttonRef,
@@ -17,10 +17,10 @@ export const Select: React.FC<Props> = (props) => {
     options,
     selectedOption,
     setSelectedOption,
-    onFocusOption,
+    onFocusOption
   } = useA11ySelect(
-    props.options.map<Option>((label) => ({ id: label, label })),
-    (option) => option.id
+    props.options.map<Option>(label => ({ id: label, label })),
+    option => option.id
   );
 
   return (
@@ -36,30 +36,35 @@ export const Select: React.FC<Props> = (props) => {
         id="combo1"
         role="combobox"
         tabIndex={0}
-        onClick={() => setIsOpen(!isOpen)}
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
       >
-        {selectedOption ? selectedOption.label : "Select an option"}
-        <ul
-          id="listbox1"
-          role="listbox"
-          aria-labelledby="combo1-label"
-          tabIndex={-1}
-          style={{ display: isOpen ? "block" : "none" }}
-          ref={optionListRef}
-        >
-          {options.map((option) => (
-            <li
-              key={option.id}
-              role="option"
-              tabIndex={option.tabIndex}
-              aria-selected={selectedOption?.id === option.id}
-              onClick={() => setSelectedOption(option.id)}
-              ref={onFocusOption(option)}
-            >
-              {option.label}
-            </li>
-          ))}
-        </ul>
+        {selectedOption ? selectedOption.label : 'Select an option'}
+        {isOpen && (
+          <ul
+            id="listbox1"
+            role="listbox"
+            aria-labelledby="combo1-label"
+            tabIndex={-1}
+            ref={optionListRef}
+          >
+            {options.map(option => (
+              <li
+                key={option.id}
+                role="option"
+                tabIndex={option.tabIndex}
+                aria-selected={selectedOption?.id === option.id}
+                onClick={() => {
+                  setSelectedOption(option.id);
+                }}
+                ref={onFocusOption(option)}
+              >
+                {option.label}
+              </li>
+            ))}
+          </ul>
+        )}
       </button>
     </div>
   );
